687e05e5df97f82f2faad9eebfa583141d9880bc
limengyuan <mengyuan.li@my.jcu.edu.au>
2018-11-27 11:40:41 +0800

    add commit

---
+++ 1.py
@@ -0,0 +1,47 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    incomes = []
+    months = int(input("How many months? "))
+
+    for month in range(1, months + 1):
+        income = float(input("Enter income for month " + str(month) + ": "))
+        incomes.append(income)
+
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, months + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
+
+
+
+
+numbers = []
+
+for i in range(5):
+    number = int(input("Number: "))
+    numbers.append(number)
+
+print("The first number is", numbers[0])
+print("The last number is", numbers[-1])
+print("The smallest number is", min(numbers))
+print("The largest number is", max(numbers))
+print("The average of the numbers is", sum(numbers) / len(numbers))
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("What is your username?")
+if username in usernames:
+    print("Access granted!")
+else:
+    print("Access denied!")
+
---
+++ quick_pick.py
@@ -0,0 +1,25 @@
+NUMBERS_PER_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
+
+import random
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("Please enter positive number of quick pick!")
+        number_of_quick_picks = int(input("How many quick picks? "))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = []
+        for j in range(NUMBERS_PER_LINE):
+            number = random.randint(MINIMUM, MAXIMUM)
+            while number in quick_pick:
+                number = random.randint(MINIMUM, MAXIMUM)
+            quick_pick.append(number)
+        quick_pick.sort()
+
+        print(" ".join("{:2}".format(number) for number in quick_pick))
+
+
+main()
